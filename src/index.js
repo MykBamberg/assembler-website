5dcb6cd0082d4b8b347d5a3825834a7a3e667933
MykBamberg <m.a.bamberg.dev@proton.me>
2025-04-11 03:01:37 +0200

    Add license statements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2025  Mykolas Bamberg
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 function assemble(source) {
     let output_paragraph = document.getElementById('asm-output');
     let simulator_link = document.getElementById('simulator-link');
@@ -101,6 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
             editor.setValue(atob(code_parameter.replace(/-/g, '+').replace(/_/g, '/')));
         }
     }
+
     /* Log toggles */
     document.querySelectorAll('#show-info,#show-warn,#show-error').forEach((element) => {
         element.checked = true;
